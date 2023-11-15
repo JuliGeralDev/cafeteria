9a69cc4474ce73c794610c10706cd43bede75a67
😀 Juliana García <jgarcia@viajemos.com>
2023-11-15 17:10:55 -0500

    Agregando configuraciones iniciales de gulp y sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 const { src, dest, watch, series, parallel} = require('gulp');
+
+//css ssas
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+//Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif')
 
 function css(done){
     //Compilar sass
@@ -16,17 +23,42 @@ function css(done){
 
 }
 
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe( dest('build/img') )
+}
+
+function versionWebp(){
+    const opciones={
+        quality: 50 
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+}
+
+function versionAvif(){
+    const opciones={
+        quality: 50 
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+}
+
 function dev(){
     watch('src/scss/**/*.scss', css);
-    // watch('src/scss/app.scss', css); //Archivo y función
+    watch('src/img/**/*', imagenes); //Archivo y función
 
 }
 
-
-
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev);
 // exports.default = parallel( css, dev);
 
 
